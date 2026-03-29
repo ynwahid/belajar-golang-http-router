0b5414c5d498f2c2ce81a222fc96943eea317584
ynwahid <yusufedu99@gmail.com>
2026-03-29 08:27:09 +0700

    test: :white_check_mark: add unit test for http router

---
+++ main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Router(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		fmt.Fprint(writer, "Hello World")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello World", string(body))
+}
