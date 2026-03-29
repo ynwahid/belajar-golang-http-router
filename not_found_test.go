05f83dceafcdbf74fbf855c62effd91502b39b49
ynwahid <yusufedu99@gmail.com>
2026-03-29 09:22:51 +0700

    feat: :sparkles: not found handler

---
+++ not_found_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NotFound(t *testing.T) {
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Gak Ketemu")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Gak Ketemu", string(body))
+}
