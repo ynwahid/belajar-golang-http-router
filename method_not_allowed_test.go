eff2f79d8c5c3c02f0ccc5f3023a7a0b898fe4bd
ynwahid <yusufedu99@gmail.com>
2026-03-29 09:28:18 +0700

    feat: :sparkles: method not allowed handler

---
+++ method_not_allowed_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_MethodNotAllowed(t *testing.T) {
+	router := httprouter.New()
+
+	router.MethodNotAllowed = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Gak Boleh")
+	})
+
+	router.POST("/", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		fmt.Fprint(writer, "POST")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Gak Boleh", string(body))
+}
